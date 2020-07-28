85e3dc37469e04798232a4a46ef1e3b95783d28b
Anand Raj <raj050300@gmail.com>
2020-07-28 13:16:34 +0530

    adMOB

--- app/src/main/java/com/armscreation/a_games/fragment/game.java
+++ app/src/main/java/com/armscreation/a_games/fragment/game.java
@@ -1,18 +1,26 @@
 package com.armscreation.a_games.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.armscreation.a_games.R;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 
 public class game extends Fragment {
-
 
     private TextView textView;
 
@@ -31,7 +39,48 @@ public class game extends Fragment {
     }
 
     private void initView(View view){
+        MobileAds.initialize(getContext(), new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {}
+        });
+
+
+
+        final InterstitialAd mInterstitialAd = new InterstitialAd(getContext());
+        mInterstitialAd.setAdUnitId("ca-app-pub-4290412064319640/1240421790");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                Toast.makeText(getContext(), "onAdLoaded()", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                Toast.makeText(getActivity(),
+                        "onAdFailedToLoad() with error code: " + errorCode,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdClosed() {
+
+            }
+        });
+
         textView = view.findViewById(R.id.fm_content_text);
         textView.setText("content");
+        Button mMyButton=view.findViewById(R.id.mMyButton);
+        mMyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                } else {
+                    Log.d("TAG", "The interstitial wasn't loaded yet.");
+                }
+            }
+        });
     }
 }
