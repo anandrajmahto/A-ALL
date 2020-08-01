36d78d8fcccfcb37bd8606cf1aa189b44885e850
anand <raj050300@gmail.com>
2020-08-01 16:40:08 +0530

    balls added

--- app/src/main/java/com/armscreation/a_games/chain/MainActivity.java
+++ app/src/main/java/com/armscreation/a_games/chain/MainActivity.java
@@ -17,9 +17,10 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth fAuth;
-    Button button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15, button16, button17, button18, button19, button20, button21, button22, button23, button24, button25,button26 ,button27 ,button28 ,button29 ,button30 ,button31 ,button32 ,button33 ,button34 ,button35 ,button36 ,button37 ,button38 ,button39 ,button40 ,button41 ,button42 ,button43 ,button44 ,button45 ,button46 ,button47 ,button48 ,button49 ,button50;
+    Button button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15, button16, button17, button18, button19, button20, button21, button22, button23, button24, button25, button26, button27, button28, button29, button30, button31, button32, button33, button34, button35, button36, button37, button38, button39, button40, button41, button42, button43, button44, button45, button46, button47, button48, button49, button50;
     Button[] array = new Button[51];
     private FirebaseAuth mAuth;
+    private int turn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         btn_multiplayer.setVisibility(View.INVISIBLE);
         mAuth = FirebaseAuth.getInstance();
         fAuth = FirebaseAuth.getInstance();
-
+        turn = 1;
 
         mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
@@ -58,8 +59,422 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        initialize();
+        onclicklistners();
+        turnchk();
 
 
+    }
+
+    private void onclicklistners() {
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(0);
+
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(1);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(2);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(3);
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(4);
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(5);
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(6);
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(7);
+            }
+        });
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(8);
+            }
+        });
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(9);
+
+            }
+        });
+        button11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(10);
+
+            }
+        });
+        button12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(11);
+
+            }
+        });
+        button13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(12);
+
+            }
+        });
+        button14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(13);
+
+            }
+        });
+        button15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(14);
+
+            }
+        });
+        button16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(15);
+
+            }
+        });
+        button17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(16);
+
+            }
+        });
+        button18.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(17);
+
+            }
+        });
+        button19.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(18);
+
+            }
+        });
+        button20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(19);
+
+            }
+        });
+        button21.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(20);
+
+            }
+        });
+        button22.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(21);
+
+            }
+        });
+        button23.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(22);
+
+            }
+        });
+        button24.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(23);
+
+            }
+        });
+        button25.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(24);
+
+            }
+        });
+        button26.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(25);
+
+            }
+        });
+        button27.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(26);
+
+            }
+        });
+        button28.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(27);
+
+            }
+        });
+        button29.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(28);
+
+            }
+        });
+        button30.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(29);
+
+            }
+        });
+        button31.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(30);
+
+            }
+        });
+        button32.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(31);
+
+            }
+        });
+        button33.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(32);
+
+            }
+        });
+        button34.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(33);
+
+            }
+        });
+        button35.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(34);
+
+            }
+        });
+        button36.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(35);
+
+            }
+        });
+        button37.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(36);
+
+            }
+        });
+        button38.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(37);
+
+            }
+        });
+        button39.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(38);
+
+            }
+        });
+        button40.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(39);
+
+            }
+        });
+        button41.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(40);
+
+            }
+        });
+        button42.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(41);
+
+            }
+        });
+        button43.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(42);
+
+            }
+        });
+        button44.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(43);
+
+            }
+        });
+        button45.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(44);
+
+            }
+        });
+        button46.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(45);
+
+            }
+        });
+        button47.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(46);
+
+            }
+        });
+        button48.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(47);
+
+            }
+        });
+        button49.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(48);
+
+            }
+        });
+        button50.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn(49);
+
+            }
+        });
+
+
+    }
+
+    void btn(int j) {
+
+        //turn 1=red
+        //turn 2=green
+
+
+        if (turn == 1 && array[j].getTag().equals(" ")) {
+            array[j].setBackgroundResource(R.drawable.red);
+            array[j].setTag("red");
+            turn = 2;
+        } else if (turn == 2 && array[j].getTag().equals(" ")) {
+            array[j].setBackgroundResource(R.drawable.green);
+            array[j].setTag("green");
+            turn = 1;
+        }
+
+        else if (turn == 1 && array[j].getTag().equals("red")) {
+            array[j].setBackgroundResource(R.drawable.red2);
+            array[j].setTag("red2");
+            turn = 2;
+        } else if (turn == 2 && array[j].getTag().equals("green")) {
+            array[j].setBackgroundResource(R.drawable.green2);
+            array[j].setTag("green2");
+            turn = 1;
+        }
+
+        else if (turn == 1 && array[j].getTag().equals("red2")) {
+            array[j].setBackgroundResource(R.drawable.red3);
+            array[j].setTag("red3");
+            turn = 2;
+        } else if (turn == 2 && array[j].getTag().equals("green2")) {
+            array[j].setBackgroundResource(R.drawable.green3);
+            array[j].setTag("green3");
+            turn = 1;
+        }
+
+
+        else if (turn == 1 && array[j].getTag().equals("red3")) {
+            array[j].setBackgroundResource(R.drawable.red4);
+            array[j].setTag("red4");
+            turn = 2;
+        } else if (turn == 2 && array[j].getTag().equals("green3")) {
+            array[j].setBackgroundResource(R.drawable.green4);
+            array[j].setTag("green4");
+            turn = 1;
+        }
+
+       turnchk();
+        // Toast.makeText(this, "Button " + (j + 1) + " Clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    private void turnchk() {
+        //turn check
+        if (turn == 1) {
+         //   Toast.makeText(this, "Red's turn", Toast.LENGTH_SHORT).show();
+        } else if (turn == 2) {
+          //  Toast.makeText(this, "Green's turn", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void initialize() {
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
@@ -135,7 +550,34 @@ public class MainActivity extends AppCompatActivity {
         array[22] = button23;
         array[23] = button24;
         array[24] = button25;
-
+        array[25] = button26;
+        array[26] = button27;
+        array[27] = button28;
+        array[28] = button29;
+        array[29] = button30;
+        array[30] = button31;
+        array[31] = button32;
+        array[32] = button33;
+        array[33] = button34;
+        array[34] = button35;
+        array[35] = button36;
+        array[36] = button37;
+        array[37] = button38;
+        array[38] = button39;
+        array[39] = button40;
+        array[40] = button41;
+        array[41] = button42;
+        array[42] = button43;
+        array[43] = button44;
+        array[44] = button45;
+        array[45] = button46;
+        array[46] = button47;
+        array[47] = button48;
+        array[48] = button49;
+        array[49] = button50;
+        for (int i = 0; i < 50; i++) {
+            array[i].setTag(" ");
+        }
     }
 
 
